4cda1935db402530dce305ec283ca4a3e4818e00
liubin595338764 <liubin595338764@163.com>
2022-01-19 22:43:41 +0800

    登录页面背景图标

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="loginPage">
+        <leftVue class="left-image" />
+        <rightVue class="right-image" />
         <p>登录</p>
         <div class="btn">
             <button @click="loginHandler">
@@ -10,6 +12,8 @@
 </template>
 
 <script setup>
+import leftVue from './components/left.vue';
+import rightVue from './components/right.vue';
 import {login} from '@/api/user.js';
 const loginHandler = ()=>{
     login().then(res=>{
@@ -28,5 +32,60 @@ const loginHandler = ()=>{
 }
 .btn {
     margin-top: 100px;
+}
+
+@media only screen and (max-width: 1920px) {
+    .left-image {
+        right: calc(50% + 270px);
+        width: calc(50% - 270px);
+        min-width: 440px;
+    }
+    .right-image {
+        left: calc(50% + 270px);
+        width: calc(50% - 270px);
+        min-width: 440px;
+    }
+}
+
+@media only screen and (min-width: 1920px) {
+    .left-image {
+        left: 0;
+        width: 700px;
+    }
+    .right-image {
+        right: 0;
+        width: 700px;
+    }
+}
+.left-image {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+}
+.right-image {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+@media only screen and (max-width: 1023px) {
+    .banner {
+        position: static;
+        text-align: left;
+        padding-left: 16px;
+        transform: none;
+        padding-top: 60px;
+        padding-right: 16px;
+        min-height: 550px;
+        height: calc(100vh - 124px);
+    }
+    .left-image {
+        position: relative;
+        left: -16px;
+        min-width: unset;
+        width: 300px;
+        top: 8px;
+        transform: none;
+    }
 }
 </style>
